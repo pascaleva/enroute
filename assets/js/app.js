b2a664a6b9ef1364ddcbc288a758061820cda67c
Pascale Anderegg <pascaleanderegg@icloud.com>
2018-01-12 21:49:52 +0100

    Bugs mit Bildern Texten angeschaut, Platzierung

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,20 @@
-
-
-
 $(document).ready(function() {
-
-
   $('#fullpage').fullpage({
-
     scrollOverflow: true,
-
-
   	afterLoad: function(anchorLink, index){
   		var loadedSection = $(this);
+      console.log("Section=" + index);
+      if(index==4) {
+         $('#nav').css("visibility", "visible");
+      }
 
-      console.log(loadedSection);
-      console.log(index)
-
-
+      if(index<4) {
+        $('#nav').css("visibility", "hidden");
+      }
   	}
   });
 
-  //
-  // // When the user scrolls the page, execute myFunction
-  // window.onscroll = function() {
-  //   alert("onscroll");
-  //   console.log("hoi muesli");
-  //   myFunction();
-  // };
-  //
-  // // Get the nav
-  // var nav = document.getElementById("nav");
-  //
-  // // Get the offset position of the nav
-  // var sticky = nav.offsetTop;
-  //
-  // // Add the sticky class to the nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  // function myFunction() {
-  //   alert("hello");
-  //   if (window.pageYOffset >= sticky) {
-  //     nav.classList.add("sticky")
-  //   } else {
-  //     nav.classList.remove("sticky");
-  //   }
-  // }
 
+  
 
 });
